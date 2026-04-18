df3fa79eac4abb3d2c31f7c2263ceb9953213172
andygrigs <andygrigs88@gmail.com>
2026-04-18 11:10:47 +0200

    Remove old Terraform and Helm configurations for Django application and replace with new structure
    - Deleted old Helm chart templates including HPA, Service, and Deployment configurations.
    - Removed outdated values.yaml and main.tf files.
    - Eliminated old ECR, EKS, and VPC modules and their associated resources.
    - Introduced new Terraform modules for ECR, EKS, and VPC with updated configurations.
    - Added new Jenkins module for CI/CD integration.
    - Created new S3 backend module for Terraform state management.
    - Added new ConfigMap and updated Deployment configurations for the Django application.
    - Established new outputs for ECR and EKS resources.
    - Created new .gitignore and .terraform.lock.hcl files for better project management.

--- practic-8/outputs.tf
+++ practic-8/outputs.tf
@@ -9,3 +9,12 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+output "jenkins_release" {
+  value = module.jenkins.jenkins_release_name
+}
+
+output "jenkins_namespace" {
+  value = module.jenkins.jenkins_namespace
+}
+
