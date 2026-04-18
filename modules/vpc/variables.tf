cf48082378ec68a858573929993b09f327f7d47c
andygrigs <andygrigs88@gmail.com>
2026-04-18 11:44:45 +0200

    finish vpc modur

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,28 @@
+variable "project_name" {
+  description = "Project name used for tagging resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets (NAT Gateway, Load Balancer)"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets (EKS worker nodes)"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
